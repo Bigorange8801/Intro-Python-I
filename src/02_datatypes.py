4a6166496a482ab55ee3bc1086754507a1b391bb
John <cjd8801@gmail.com>
2020-07-07 10:12:29 -0500

    0-3

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,10 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+combine = int(x) + int(y)
+print(combine)
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(str(x) + y)
--- src/03_modules.py
+++ src/03_modules.py
@@ -10,13 +10,15 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+for line in sys.argv:
+    print("This line" + line)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print("My os" + str(os.name))
+print("My platform" + str(platform.system()))
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print("Py version" + sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
